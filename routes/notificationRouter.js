d3f7680106e8dd26422b0b1e0b47c8eb865c92c6
Abdelrahman <86893144+AbdelrahmanOmar1@users.noreply.github.com>
2026-07-03 09:01:38 +0300

    Delete routes directory

--- routes/notificationRouter.js
+++
@@ -1,9 +0,0 @@
-const express= require("express");
-const router = express.Router();
-const notificationController = require('../controllers/notification');
-const authController = require('../controllers/authController');
-
-router.get("/",authController.protect,  notificationController.showNotifications);
-router.put("/:id/read",authController.protect, notificationController.markAsRead);
-
-module.exports = router;
